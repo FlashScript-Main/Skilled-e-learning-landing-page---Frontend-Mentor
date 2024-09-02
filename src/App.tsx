a8f26ca6ffe2e75da91989d4b8cbaea004ba6039
FlashScript <itzflashscript@gmail.com>
2024-09-02 17:25:53 +0330

    Adding Needed Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import { Features, FlashScript, Footer, Header, Hero, Testimonial,  } from "./components"
+import { Features, FlashScript, Footer, Header, Hero } from "./components"
 
 const App = () => {
 
     return (
         <div className="w-full">
-            <section className={`w-full | bg-primary-dark-purple |  | `}>
-                <Header />
-            </section>
+            <Header />
 
             <Hero />
 
             <Features />
-
-            <Testimonial />
-
+            
             <Footer />
             
             <FlashScript />
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,16 +2,12 @@ import FlashScript from "./FlashScript";
 import Header from "./Header";
 import Hero from "./Hero";
 import Features from "./Features";
-import Testimonial from "./Testimonial";
 import Footer from "./Footer";
-import Button from "./Button";
 
 export {
     FlashScript,
     Header,
     Hero,
     Features,
-    Testimonial,
-    Footer,
-    Button
+    Footer
 }
