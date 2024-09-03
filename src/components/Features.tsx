b136f8c42da11363225658f1ac36961ce123ca2a
FlashScript <itzflashscript@gmail.com>
2024-09-03 12:01:50 +0330

    Website's Design is Done!

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -4,9 +4,9 @@ import GetStartedButton from "./GetStartedButton"
 const Features = () => {
 
     return (
-        <section className={`mx-auto max-w-[94%] md:max-w-[44.5rem] mb-[4.5rem] md:mt-[11.5rem] |  | grid grid-cols-1 gap-y-12 md:grid-cols-2 md:gap-x-2 md:gap-y-14 lg:grid-cols-3 | `}>
+        <section className={`lg:px-6 mx-auto max-w-[94%] md:max-w-[44.5rem] lg:max-w-[65rem] mb-[4.5rem] md:mt-[11.5rem] lg:mt-[15rem] lg:mb-[6rem] |  | grid grid-cols-1 gap-y-12 md:grid-cols-2 md:gap-x-2 md:gap-y-14 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-[4.5rem] | `}>
             <div className={`pb-8 pt-6 md:pt-14 px-8 | gradient-magenta-orange-2 |  | rounded-xl`}>
-                <h2 className={` | text-2xl text-neutral-white font-bold  |  | `}>
+                <h2 className={` | text-2xl lg:text-[1.7rem] lg:leading-[2.2rem] text-neutral-white font-bold  |  | `}>
                     {featureCourse}
                 </h2>
             </div>
@@ -15,7 +15,7 @@ const Features = () => {
                 featureInfo.map((f) => (
                     <div 
                         key={f.id}
-                        className={`h-[14.8rem] px-6 md:px-8 relative | bg-neutral-white |  | rounded-xl shadow-lg shadow-primary-dark-blue-gray/10`}
+                        className={`h-[14.8rem] lg:h-[17rem] px-6 md:px-8 lg:pr-6 relative | bg-neutral-white |  | rounded-xl shadow-lg shadow-primary-dark-blue-gray/10`}
                     >
                         <img 
                             src={f.featureIcon} 
@@ -23,11 +23,11 @@ const Features = () => {
                             className={`absolute top-[-1.8rem] left-[1.7rem] md:left-[2rem] |  |  | `}
                         />
 
-                        <h4 className={`mt-11 mb-3 | text-primary-yankees-blue text-xl font-black |  | `}>
+                        <h4 className={`mt-11 lg:mt-[3.3rem] mb-3 lg:mb-5 | text-primary-yankees-blue text-xl lg:text-[1.4rem] font-black |  | `}>
                             {f.featureTitle}
                         </h4>
 
-                        <p className={`max-h-[5rem] | text-base text-primary-silver font-medium |  | `}>
+                        <p className={`max-h-[5rem] lg:max-h-[4rem] | text-base text-primary-silver font-medium |  | `}>
                             {f.featureDescription}
                         </p>
 
@@ -35,7 +35,7 @@ const Features = () => {
                             textColor="text-primary-pink font-extrabold"
                             onHover="hover:text-primary-carnation-pink"
                             px=""
-                            py="pt-6"
+                            py="pt-6 lg:pt-12"
                         />
                     </div>
                 ))
